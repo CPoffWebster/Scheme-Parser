b1a40e45ce5f315f9ba2cea9292d4f85d6b11b41
CPOFFWEBSTER <cpoffwebster@clarku.edu>
2019-02-19 13:03:11 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Part1
+//
+//  Created by Charlie Poff-Webster on 2/19/19.
+//  Copyright © 2019 Charlie Poff-Webster. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
